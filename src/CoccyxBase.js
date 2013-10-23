b72b5ff632ff88ce90f1e6a5b28421967f1763bc
Vitaly Kramskikh <vkramskikh@mirantis.com>
2013-10-23 18:18:28 +0400

    Added handling of deferreds

--- src/CoccyxBase.js
+++ src/CoccyxBase.js
@@ -56,16 +56,37 @@
       delete this.subViews[subView.cid];
     },
 
+    registerDeferred: function(deferred) {
+      var that = this;
+      this.deferreds = this.deferreds || {};
+      deferred._coccyxId = deferred._coccyxId || _.uniqueId('coccyx');
+      this.deferreds[deferred._coccyxId] = deferred;
+      deferred.always(function() {
+        delete that.deferreds[deferred._coccyxId];
+      });
+      return deferred;
+    },
+
+    unregisterDeferred: function(deferred) {
+      delete this.deferreds[deferred._coccyxId];
+    },
+
     tearDown: function() {
       this._tearDown();
       this.$el.remove();
       return this;
     },
-    
+
     tearDownRegisteredSubViews: function() {
       _(this.subViews).invoke('_tearDown');
     },
-    
+
+    rejectRegisteredDeferreds: function() {
+      _(this.deferreds).each(function(deferred) {
+        deferred[_(['abort', 'clear', 'reject']).find(function(method) {return deferred[method]})]();
+      });
+    },
+
     _tearDown: function() {
       var that = this;
       if (this.beforeTearDown) this.beforeTearDown();
@@ -78,7 +99,10 @@
 
       _(this.eventDispatchers).invoke('off', null, null, this);
       this.eventDispatchers = {};
-      
+
+      this.rejectRegisteredDeferreds();
+      this.deferreds = {};
+
       _(this.subViews).invoke('_tearDown');
       this.subViews = {};
     }
